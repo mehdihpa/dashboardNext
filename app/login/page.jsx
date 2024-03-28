1277e188642b0c2cb27cb81ec258ba52f8cacd99
Mehdi <mehdi@Mehdis-MacBook-Air.local>
2024-03-28 13:59:16 +0330

    modify loginForm

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,5 @@
 import styles from "@/app/ui/login/login.module.css";
-import LoginForm from "@/app/ui/login/loginForm/loginform";
+import LoginForm from "@/app/ui/login/loginForm";
 const LoginPage = () => {
   return (
     <div className={styles.container}>
